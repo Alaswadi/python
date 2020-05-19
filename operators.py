84a2f0b839c375d7f4f619b124bd75d29cb4fad6
Fadi AL-Aswadi <fadi.alaswadi@gmail.com>
2020-05-20 00:49:37 +0300

    operators

---
+++ operators.py
@@ -0,0 +1,29 @@
+num1 = 5
+num2 = 6
+print(num1 > num2)
+print(num1 < num2)
+print (5 == 6)
+print (5 == 5)
+print (5 != 6)
+print (5 >= 6)
+print (7 >= 6)
+print (5 <= 6)
+
+print(True and True) # True
+print(True and False) #False
+print(False and False) #False
+a,b = 1,2
+print(a == a and b == b) #true
+print (a == a and b < a) #false
+print(a>b and b<a) #false
+print(True or True) #true
+print(True or False) #true
+print(False or False) #false
+print (a == a or b == b) #true
+print (a == a or b < a) #true
+print (a > b or b < a) #false
+
+print (not True) #false
+print(not False) #true
+print(not (a != 5)) #false
+print(not (a == 5))
