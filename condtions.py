c74e8d2df44c10f6733c2b7150f425bf86ec043b
Fadi AL-Aswadi <fadi.alaswadi@gmail.com>
2020-05-21 01:36:28 +0300

    condtions

---
+++ condtions.py
@@ -0,0 +1,6 @@
+username = input('Please insert your userName: ')
+password = input('Please insert your password: ')
+if username == 'fadi' and password == '123':
+    print('Welcome to your email')
+else:
+    print('Error')
