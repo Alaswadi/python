b229b7897413e21a6b38e78e3f97cbd40f00e1fb
Fadi AL-Aswadi <fadi.alaswadi@gmail.com>
2020-05-24 23:18:39 +0300

    Dictionary

---
+++ dictionaries.py
@@ -0,0 +1,8 @@
+subjects_with_no_student = {"Arabic":3, "English":3, "Math": 2}
+print(subjects_with_no_student)
+students = {"Arabic":["Ali","Mohammed","Mona"], "English":["Jack","Ahmed","Munif"]}
+print(students)
+students['Math'] = ['Salah','Hussein']
+print(students)
+students.update({"History":'Fadi'})
+print(students)
