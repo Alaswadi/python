0c4bf955673baece4fb5657cd2ad260f03103bf1
Fadi AL-Aswadi <fadi.alaswadi@gmail.com>
2020-05-24 23:05:31 +0300

    forloops

---
+++ forloop.py
@@ -0,0 +1,17 @@
+subjects = ['Arabic', 'English','Math']
+print(subjects)
+print(subjects[1])
+print(subjects[0])
+subjects.append('History')
+print(subjects)
+print(subjects[1:3])
+print(subjects[1:4])
+print(subjects[-1])
+# subjects.pop()
+# print(subjects)
+# subjects.pop(0)
+# print(subjects)
+
+
+for x in subjects:
+    print(x)
